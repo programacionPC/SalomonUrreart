eccd166925fe511ff82dd37e929496df7cb4cb46
PabloCabeza <pcabeza7@gmail.com>
2021-06-22 07:58:49 -0400

    cambios generales

--- application/views/header/header_DetallesPonchos.php
+++ application/views/header/header_DetallesPonchos.php
@@ -6,8 +6,8 @@
 		<meta http-equiv="content-type"  content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/EstilosPort_Art.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/MediaQuery_EstilosPort_Art_800.css"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/EstilosPort_Art.css?v=<?php echo rand();?>"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/MediaQuery_EstilosPort_Art_800.css?v=<?php echo rand();?>"/>
 		
         <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=RLato|Raleway:400|Montserrat|Gruppo'>
         
